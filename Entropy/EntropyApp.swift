46584ce40e896e561d1060d00d4c3e02dca267a0
Khoa Phan <khoaphancontact@gmail.com>
2025-12-05 23:52:35 -0800

    Initial Commit

---
+++ Entropy/EntropyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntropyApp.swift
+//  Entropy
+//
+//  Created by Khoa Phan (Home) on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct EntropyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
